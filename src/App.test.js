94db1ef3f95df06112a84074d1159b72f74af0fc
Daniel <59184832+daniel-moderiano@users.noreply.github.com>
2022-08-19 18:05:29 +0930

    Adjust App.test.js to conform to linting rules

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders App', () => {
   render(
     <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter>,
   );
 });
