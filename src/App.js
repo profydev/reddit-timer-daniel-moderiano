12f09ad49193f927644782a059a64609011ed4b4
Daniel <59184832+daniel-moderiano@users.noreply.github.com>
2022-08-19 17:35:16 +0930

    Add routes and placeholder components for home and search page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import GlobalStyles from './components/globalStyles';
+import Search from './components/Search/Search';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div>
       <GlobalStyles />
       App Placeholder
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
     </div>
   );
 }
