3e7b24b2642ab1d777bf99a7d0afcf4b21661a32
Daniel <59184832+daniel-moderiano@users.noreply.github.com>
2022-08-25 10:06:41 +0930

    Add font-face declarations to globalStyles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -6,6 +6,45 @@ const GlobalStyles = createGlobalStyle`
    License: none (public domain)
 */
 
+  /** Montserrat Regular **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 400;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Regular.woff2") format("woff2"), url("/fonts/Montserrat-Regular.woff") format("woff");
+  }
+
+  /** Montserrat Medium **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 500;
+    font-style: normal;
+    src: url("/fonts/Montserrat-Medium.woff2") format("woff2"), url("/fonts/Montserrat-Medium.woff") format("woff");
+  }
+
+  /** Montserrat SemiBold **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 600;
+    font-style: normal;
+    src: url("/fonts/Montserrat-SemiBold.woff2") format("woff2"), url("/fonts/Montserrat-SemiBold.woff") format("woff");
+  }
+
+  /** Montserrat Bold **/
+  @font-face {
+    font-family: "Montserrat";
+    font-weight: 700;
+    font-style: normal;
+    src: url("Montserrat-Bold.woff2") format("woff2"), url("Montserrat-Bold.woff") format("woff");
+  }
+
+  /** Bitter Regular **/
+  @font-face {
+    font-family: "Bitter";
+    src: url("/fonts/Bitter-Regular.woff2") format("woff2"),
+         url("/fonts/Bitter-Regular.woff") format("woff");
+  }
+
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
@@ -25,6 +64,7 @@ const GlobalStyles = createGlobalStyle`
     font-size: 100%;
     font: inherit;
     vertical-align: baseline;
+    font-family: 'Bitter';
   }
   /* HTML5 display-role reset for older browsers */
   article, aside, details, figcaption, figure, 
