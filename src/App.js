f704e922a56118bd15ac5625d814abc72fe2b5ae
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2022-08-27 18:22:45 +0930

    Create app skeleton (#1)
    * Create global styles using styled components, with a reset CSS added
    
    * Add CSS imports for fonts Bitter and Montserrat
    
    * Add routes and placeholder components for home and search page
    
    * Move CSS font imports to App.css file, and amend App.test.js to include Routing context
    
    * Adjust App.test.js to conform to linting rules
    
    * Add public/fonts directly with woff font files for Bitter and Montserrat
    
    * Add font-face declarations to globalStyles
    
    * Remove App.css
    
    * Modify global styles to include normalize.css instead of reset CSS
    
    * Add theme provider with theming using example implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './globalStyles';
+import Search from './components/Search/Search';
+import Home from './components/Home/Home';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
